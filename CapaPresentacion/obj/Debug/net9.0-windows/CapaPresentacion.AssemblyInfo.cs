28dfa2c6dc046ea7394aa780e9e254f4f8f7f2ec
Jesús Abraham Zapata Estrada <jesus.zapata@uabc.edu.mx>
2026-04-12 19:32:03 -0700

    Ajustes en el archivo cicd-test.yml
    Se hicieron unos ajustes en el archivo de las pruebas automatizadas cicd-test.yml

--- CapaPresentacion/obj/Debug/net9.0-windows/CapaPresentacion.AssemblyInfo.cs
+++ CapaPresentacion/obj/Debug/net9.0-windows/CapaPresentacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4acfb22525cd230ae4967554f116be048bf1f808")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39fff6447f9e45272d4671a9e54ee56ce03cf461")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
